b4c2dd6611e19518d9669b38afca2a0263fba9c3
fanzhiqiang <1686636056@qq.com>
2020-07-31 21:44:33 +0800

    项目初始化

---
+++ vue-config.js
@@ -0,0 +1,13 @@
+export default {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
+}
